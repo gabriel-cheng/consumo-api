29f6fde179560b943fe5ef74cd46636cff207956
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2022-09-15 17:20:06 -0300

    :ok_hand: Style - Finalizando parte lógica de API

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
-const tabela = document.querySelector('#tabela');
+const nomeMoeda = document.querySelector('#nome');
+const valorMoeda = document.querySelector('#valor');
+const time = document.querySelector('#time');
 
 function getDados(url) {
     const request = new XMLHttpRequest();
     request.open("GET", url, false);
     request.send();
-
-    return request.responseText;
-}
-
-function criarLinha(moeda) {
-    linha = document.createElement('tr');
-    tdId = document.createElement('td');
-    tdNome = document.createElement('td');
-    // tdId.innerHTML = moeda.id;
-    tdNome.innerHTML = moeda;
     
-    console.log(moeda);
-
-    linha.appendChild(tdId);
-    linha.appendChild(tdNome);
-
-    return linha;
+    return request.response;
 }
 
-(function main() {
+setInterval(() => {
     let data = getDados('http://economia.awesomeapi.com.br/json/last/USD-BRL');
     let moeda = JSON.parse(data);
     moeda = moeda.USDBRL;
 
-    Object.keys(moeda).forEach(key => {
-        let linha = criarLinha(key);
-        tabela.appendChild(linha);
-    });
-})();
+    nomeMoeda.textContent = moeda.name;
+    valorMoeda.textContent = moeda.bid
+    time.textContent = moeda.create_date
+}, 100);
